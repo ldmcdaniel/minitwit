c9d036b4ba2ac5896b09c68168b055954dfbd9ab
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-08-20 10:19:02 -0500

    set up tests based on trello board

---
+++ app/pagination/test/test.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var should = require('chai').should();
+var Animal = require(path.join(process.cwd(), '/lib/Pagination'));
+
+describe('Pagination', function () {
+
+  describe('Home Page', function () {
+
+  });
+
+  describe('View older posts', function () {
+
+  });
+
+  describe('View older posts', function () {
+
+  });
+
+  describe('View newer posts', function () {
+
+  });
+
+});
