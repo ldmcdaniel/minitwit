e8ef667b97d4847b4c18cead5af0449c5405361f
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-08-21 11:42:36 -0500

    working on tests

--- app/pagination/test/test.js
+++ app/pagination/test/test.js
@@ -3,21 +3,30 @@ var should = require('chai').should();
 var Animal = require(path.join(process.cwd(), '/lib/Pagination'));
 
 describe('Pagination', function () {
+  it.should('show amaximum of 10 UserTwit posts ', function () {
+
+  });
+  and.it.should('be sorted with the most recent posts descending from top', function () {
+
+  });
 
   describe('Home Page', function () {
 
+    and.it.should('show a View older posts button', function () {
+
+    })
   });
 
   describe('View older posts', function () {
+    it.should('link to posts that are 10 posts older than the current view', function () {
 
-  });
-
-  describe('View older posts', function () {
-
+    });
   });
 
   describe('View newer posts', function () {
+    it.should('link to posts that are 10 posts newer than the current view', function () {
 
+    });
   });
 
 });
